eb1e87888876cbc5281cfac58d3c1f376658397e
Johannes Weber <johannes.k.weber@web.de>
2022-03-21 22:11:16 +0100

    Added JSON-Array questions & designing the question cards

---
+++ script.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        "question": "Wovon spricht ein Pfälzer, wenn er Grumbeere kaufen möchte?",
+        "answer_1": "Erdbeere",
+        "answer_2": "Äpfel",
+        "answer_3": "Kartoffeln",
+        "answer_4": "Traube",
+        "correct_answer": 3
+    },
+    {
+        "question": "Was meint ein Bayer, wenn er von einer Radschn?",
+        "answer_1": "Eine Person, die eine Ratsche benutzt",
+        "answer_2": "Einen Radfahrer",
+        "answer_3": "Rasenmäher",
+        "answer_4": "Klatschtante",
+        "correct_answer": 4
+    },
+    {
+        "question": "Wenn sich der Badner über d’Schnoog aufregt. Was könnte das sein?",
+        "answer_1": "Stechmücke",
+        "answer_2": "Schnecke",
+        "answer_3": "Schnee",
+        "answer_4": "Schnurbart",
+        "correct_answer": 1
+    },
+    {
+        "question": "Welche Tätigkeit führt ein/e Sachse/Sächsin aus, wenn vom didschen gesprochen wird?",
+        "answer_1": "gegen etwas prallen",
+        "answer_2": "etwas eintunken",
+        "answer_3": "als DJ eine Platte auflegen",
+        "answer_4": "Eine gepflegte Dusche einnehmen",
+        "correct_answer": 1
+    },
+    {
+        "question": "Was bestellt ein/e Berliner/in, wenn ein Brühpulla bestellt wird?",
+        "answer_1": "Döner",
+        "answer_2": "Gulaschsuppe",
+        "answer_3": "Currywurst mit Brötchen",
+        "answer_4": "Bockwurst mit Senf und Brot",
+        "correct_answer": 4
+    },
+    {
+        "question": "Was bedeutet das Wort Fisematente im Saarland?",
+        "answer_1": "Besonders fiese Person",
+        "answer_2": "Schwierigkeit",
+        "answer_3": "Schmutzige Füße",
+        "answer_4": "Gemeine Tante",
+        "correct_answer": 1
+    },
+    {
+        "question": "Was mein ein Ostfriese, wenn er sagt: Bitte gib mir mal die Funzel?",
+        "answer_1": "Feuerzeug",
+        "answer_2": "Kerze",
+        "answer_3": "Tachenlampe",
+        "answer_4": "Kehrbesen",
+        "correct_answer": 3
+    }
+];
