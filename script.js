1041b4a3fe4cb06b5e7a33eae4a931d5d1f56503
Johannes Weber <johannes.k.weber@web.de>
2022-03-23 23:43:15 +0100

    First Version of QUIZ APP - GERMAN DIALECTS

--- script.js
+++ script.js
@@ -56,3 +56,103 @@ let questions = [
         "correct_answer": 3
     }
 ];
+
+
+let currentQuestion = 0;
+let correctAnswerCounter = 0;
+let AUDIO_SUCCESS = new Audio('audio/success_sound.mp3')
+let AUDIO_FAIL = new Audio('audio/fail_sound.mp3')
+
+
+function init() {
+    document.getElementById('questionLength').innerHTML = questions.length;
+    document.getElementById('endScreenLength').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion() {
+
+    //Show endscreen, when value > then array.length
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endScreen').style = '';
+        document.getElementById('questionScreen').style = 'display: none';
+        document.getElementById('endScreenCorrectAnswers').innerHTML = correctAnswerCounter;
+        document.getElementById('card-img-top').src = 'img/win2.jpg';
+    }
+
+    //Show (next) question, when value < then array.length
+    else {
+        let question = questions[currentQuestion];
+        showCurrentQuestionNo();
+        document.getElementById('questionText').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+
+        let progressPercent = (currentQuestion + 1) / questions.length;
+        progressPercent = Math.round(progressPercent * 100);
+        console.log(progressPercent);
+        document.getElementById('progressPercentage1').innerHTML = `${progressPercent} %`;
+        document.getElementById('progressPercentage1').style = `width: ${progressPercent}%`;
+        document.getElementById('progressPercentage2').innerHTML = `${progressPercent} %`;
+        document.getElementById('progressPercentage2').style = `width: ${progressPercent}%`;
+
+        showCurrentQuestionNo();
+    }
+}
+
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let correctAnswer = question['correct_answer'];
+    let selectionQuestionNumber = selection.slice(-1);
+    let idOfCorrectAnswer = 'answer_' + correctAnswer.toString();
+
+    if (selectionQuestionNumber == correctAnswer) {
+        document.getElementById(selection).parentNode.classList.add('bg-success'); //.parentNode, da der übergeordnete Container grüngefärbt werden soll.
+        AUDIO_SUCCESS.play();
+        correctAnswerCounter++;
+    }
+
+    else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfCorrectAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
+    }
+
+    document.getElementById('btnNext').disabled = false;
+}
+
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('btnNext').disabled = true;
+
+    cleanAnswers();
+    showQuestion();
+}
+
+
+function cleanAnswers() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger', 'bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger', 'bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger', 'bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger', 'bg-success');
+}
+
+
+function showCurrentQuestionNo() {
+    document.getElementById('currentQuestionNo').innerHTML = currentQuestion + 1;
+}
+
+function restartGame() {
+    document.getElementById('endScreen').style = 'display: none';
+    document.getElementById('questionScreen').style = '';
+    currentQuestion = 0;
+    correctAnswerCounter = 0;
+
+    showQuestion();
+}
